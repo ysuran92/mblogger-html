0e4a76155f431a3c06fb974a60d06c97aef8d6bb
ysuran92 <martinyanez420@gmail.com>
2020-12-30 12:28:29 -0300

    logs only once per refresh

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function logTime() {
-    document.body.innerHTML += Date() + '<br>';
+var saved = localStorage.getItem('savedTime');
+
+    if (saved) {
+    document.body.innerHTML = saved;
 }
+
+var logTime = document.querySelector("#logTimeButton")
+
+logTime.addEventListener('click', function (event) {
+    
+    document.body.innerHTML += Date() + '<br>';
+
+    localStorage.setItem('savedTime', document.body.innerHTML);
+    
+});
