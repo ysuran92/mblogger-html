5a61a0070e7e0900594161bc65e112e3d0745ca6
ysuran92 <martinyanez420@gmail.com>
2020-12-30 22:51:34 -0300

    logo, dark mode, neater

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var saved = localStorage.getItem('savedTime');
 var logTime = document.querySelector("#logTimeButton");
 var logArea = document.querySelector("#logArea");
-var clear = document.querySelector("#clear")
+var clear = document.querySelector("#clear");
+var moonbound = document.querySelector("#moonboundArea");
 
 if (saved) {
     logArea.innerHTML += saved;
-}
+};
 
-logTime.addEventListener("click", function() {;
+logTime.addEventListener("click", function() {
     logArea.innerHTML += Date() + '<br>';
     localStorage.setItem('savedTime', logArea.innerHTML);  
 });
 
 clear.addEventListener("click", function() {
     logArea.innerHTML = "";
+});
+
+moonbound.addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
 });
